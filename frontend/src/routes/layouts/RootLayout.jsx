7ba3e9735add476a374403ad145440f86e26026c
Jonathan Ramirez <jonksff@gmail.com>
2023-06-25 00:16:13 -0400

    ADD: modal to the basic layout

--- frontend/src/routes/layouts/RootLayout.jsx
+++ frontend/src/routes/layouts/RootLayout.jsx
@@ -1,16 +1,22 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../../../components/Menu/Navbar';
+import Modal from '../../../components/Modals/Modal';
 
 const RootLayout = () => {
-	return (
-		<Fragment>
-			<MainNavigation />
-			<main>
-				<Outlet />
-			</main>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+      <Modal
+        isOpen
+        title='Login'
+        actionLabel='Submit'
+      />
+    </Fragment>
+  );
 };
 
 export default RootLayout;
