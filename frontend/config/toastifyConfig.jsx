857e9a574ce23d33c14b8d78e5ee7fe54fe35cb7
Jonathan Ramirez <jonksff@gmail.com>
2023-06-25 18:01:23 -0400

    ADD toastify config

---
+++ frontend/config/toastifyConfig.jsx
@@ -0,0 +1,10 @@
+export const toastOptions = {
+	position: 'bottom-left',
+	autoClose: 1500,
+	hideProgressBar: false,
+	closeOnClick: true,
+	pauseOnHover: false,
+	draggable: true,
+	progress: undefined,
+	theme: 'dark',
+};
