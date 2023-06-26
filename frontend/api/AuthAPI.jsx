eec3ea52d267a7affd70bf170441ddb9b246b8f2
Jonathan Ramirez <jonksff@gmail.com>
2023-06-26 16:35:57 -0400

    EDIT: naming API

--- frontend/api/AuthAPI.jsx
+++ frontend/api/AuthAPI.jsx
@@ -1,12 +1,33 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { toastOptions } from '../config/toastifyConfig';
+import { ENDPOINT_REGISTER, ENDPOINT_TOKEN } from '../config/apiRoutesConfig';
 
-export const registerAPI = async (data, setIsLoading) => {
+export const registerUserAPI = async (data, setIsLoading) => {
 	try {
-		await axios.post('/api/register', data);
+		const response = await axios.post(ENDPOINT_REGISTER, data, {
+			withCredentials: false,
+		});
+		toast.success('User created', toastOptions);
+		return response;
 	} catch (error) {
 		toast.error('Something happened', toastOptions);
+		return error.response;
+	} finally {
+		setIsLoading(false);
+	}
+};
+
+export const getAuthorizationAPI = async (data, setIsLoading) => {
+	try {
+		const response = await axios.post(ENDPOINT_TOKEN, data, {
+			withCredentials: false,
+		});
+		toast.success('User logged in', toastOptions);
+		return response;
+	} catch (error) {
+		toast.error('Please check credentials.', toastOptions);
+		return error.response;
 	} finally {
 		setIsLoading(false);
 	}
