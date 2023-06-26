6f9e2bccf8b01ac649295991deded377ed18e441
Jonathan Ramirez <jonksff@gmail.com>
2023-06-26 19:55:05 -0400

    ADD: reading token local storage and get user info

--- frontend/src/routes/layouts/RootLayout.jsx
+++ frontend/src/routes/layouts/RootLayout.jsx
@@ -1,10 +1,41 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../../../components/Menu/Navbar';
 import RegisterModal from '../../../components/Modals/RegisterModal';
 import LoginModal from '../../../components/Modals/LoginModal';
+import { getUserInformationAPI } from '../../../api/AuthAPI';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../../../store/user-slice';
 
 const RootLayout = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const token = localStorage.getItem('auth_token');
+			if (!token) return;
+
+			const response = await getUserInformationAPI(token);
+
+			if (response.status === 200) {
+				const { key, user } = response.data;
+
+				dispatch(
+					userActions.loginUser({
+						token: key,
+						first_name: user.first_name,
+						last_name: user.last_name,
+						email: user.email,
+						avatar: user.avatar,
+						userID: user.id,
+					})
+				);
+			}
+		};
+
+		fetchUser();
+	}, [dispatch]);
+
 	return (
 		<Fragment>
 			<MainNavigation />
