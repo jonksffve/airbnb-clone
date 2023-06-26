f7d93e32da857f573b293c67941da8c11d87e478
Jonathan Ramirez <jonksff@gmail.com>
2023-06-26 16:25:57 -0400

    EDIT: Custom way to authenticate token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-
 
 urlpatterns = [
     path('account/', views.UserCreationView.as_view()),
-    path('account/auth/', obtain_auth_token)
+    path('account/auth/', views.CustomAuthToken.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,29 @@
 from rest_framework.generics import CreateAPIView
 from accounts.models import User
 from accounts.serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 
 
 class UserCreationView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'userID': user.pk,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'avatar': user.avatar.url,
+        })
