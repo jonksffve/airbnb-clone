5c99fd408c8cb7b6e8e46c25c341ec33887d4657
Jonathan Ramirez <jonksff@gmail.com>
2023-06-26 03:20:34 -0400

    ADD: endpoint to get token access for clients

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-# from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('account/', views.UserCreationView.as_view()),
-    # path('api-token-auth/', obtain_auth_token)
+    path('account/auth/', obtain_auth_token)
 ]
