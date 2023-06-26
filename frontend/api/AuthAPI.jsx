2933b7e3cc3ebe5da4ea6b4147609009fff9a725
Jonathan Ramirez <jonksff@gmail.com>
2023-06-26 16:59:56 -0400

    EDIT: endpoint names and directions

--- frontend/api/AuthAPI.jsx
+++ frontend/api/AuthAPI.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { toastOptions } from '../config/toastifyConfig';
-import { ENDPOINT_REGISTER, ENDPOINT_TOKEN } from '../config/apiRoutesConfig';
+import { ENDPOINT_ACCOUNT, ENDPOINT_AUTH } from '../config/apiRoutesConfig';
 
 export const registerUserAPI = async (data, setIsLoading) => {
 	try {
-		const response = await axios.post(ENDPOINT_REGISTER, data, {
+		const response = await axios.post(ENDPOINT_ACCOUNT, data, {
 			withCredentials: false,
 		});
 		toast.success('User created', toastOptions);
@@ -20,7 +20,7 @@ export const registerUserAPI = async (data, setIsLoading) => {
 
 export const getAuthorizationAPI = async (data, setIsLoading) => {
 	try {
-		const response = await axios.post(ENDPOINT_TOKEN, data, {
+		const response = await axios.post(ENDPOINT_AUTH, data, {
 			withCredentials: false,
 		});
 		toast.success('User logged in', toastOptions);
--- frontend/config/apiRoutesConfig.jsx
+++ frontend/config/apiRoutesConfig.jsx
@@ -1,2 +1,2 @@
-export const ENDPOINT_REGISTER = 'http://localhost:8000/api/account/';
-export const ENDPOINT_TOKEN = 'http://localhost:8000/api/account/auth/';
+export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
+export const ENDPOINT_AUTH = 'http://localhost:8000/api/auth/token_auth/';
