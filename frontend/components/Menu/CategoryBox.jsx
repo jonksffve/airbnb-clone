bed133ae24ffd4da4a7f2229f3cfee1e58ef2bde
Jonathan Ramirez <jonksff@gmail.com>
2023-06-27 01:03:59 -0400

    DONE: categorybox comp logic

---
+++ frontend/components/Menu/CategoryBox.jsx
@@ -0,0 +1,43 @@
+import { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const CategoryBox = ({ icon: Icon, label, selected }) => {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const handleClick = useCallback(() => {
+		if (searchParams.get('category') === label) {
+			searchParams.delete('category');
+			setSearchParams({ ...searchParams });
+			return;
+		}
+
+		setSearchParams({
+			...searchParams,
+			category: label,
+		});
+	}, [label, setSearchParams, searchParams]);
+
+	return (
+		<div
+			onClick={handleClick}
+			className={`flex
+            cursor-pointer
+            flex-col
+            items-center
+            justify-center
+            gap-2
+            border-b-2
+            p-3
+            transition
+            hover:text-neutral-800
+            ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+            ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+            `}
+		>
+			<Icon size={26} />
+			<div className='text-sm font-medium'>{label}</div>
+		</div>
+	);
+};
+
+export default CategoryBox;
