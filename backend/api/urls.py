b8dede4dda570acce4b7c8f3f8018ff01431bee0
Jonathan Ramirez <jonksff@gmail.com>
2023-06-28 05:54:10 -0400

    EDIT: class views names

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/', views.UserCreationView.as_view()),
-    path('account/<str:key>/', views.RetrieveUserInformation.as_view()),
+    path('account/', views.UserCreateView.as_view()),
+    path('account/<str:key>/', views.UserRetrieveView.as_view()),
     path('auth/token_auth/', views.CustomAuthToken.as_view()),
+    path('listing/', views.ListingCreateView.as_view()),
 ]
