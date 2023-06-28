8100d31f749b4c0357a981b0f7a926fb7f5c7234
Jonathan Ramirez <jonksff@gmail.com>
2023-06-27 21:59:25 -0400

    ADD: select to rentalmodal

---
+++ frontend/components/UIhelpers/Inputs/CountrySelect.jsx
@@ -0,0 +1,34 @@
+import { Select, Space } from 'antd';
+import { getAll } from '../../../hooks/WorldCountries';
+
+const CountrySelect = ({ onSelect, value }) => {
+	const countries = getAll();
+	const { Option } = Select;
+
+	return (
+		<Select
+			placeholder='Select anywhere in the world!'
+			value={value}
+			onChange={(value) => {
+				onSelect(value);
+			}}
+			allowClear
+		>
+			{countries.map((country) => (
+				<Option
+					key={country.label}
+					value={country.value}
+					label={country.label}
+				>
+					<Space className='flex flex-row items-center gap-3'>
+						<span role='img'>{country.flag}</span>
+						{country.label}-
+						<span className='text-neutral-500'>{country.region}</span>
+					</Space>
+				</Option>
+			))}
+		</Select>
+	);
+};
+
+export default CountrySelect;
