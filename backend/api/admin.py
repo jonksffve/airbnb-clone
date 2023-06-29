c43705a3361ac980009fe545a78f4445087b7482
Jonathan Ramirez <jonksff@gmail.com>
2023-06-29 16:54:39 -0400

    ADD: FavoriteListing to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Listing
+from .models import Category, Listing, FavoriteListing
 
 
 admin.site.register(Category)
 admin.site.register(Listing)
+admin.site.register(FavoriteListing)
