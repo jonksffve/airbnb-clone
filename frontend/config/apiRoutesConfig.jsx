46da418dfd3728ccde2e86f8f25abb5ad824b333
Jonathan Ramirez <jonksff@gmail.com>
2023-06-29 16:56:08 -0400

    ADD: favorite endpoint

--- frontend/config/apiRoutesConfig.jsx
+++ frontend/config/apiRoutesConfig.jsx
@@ -1,3 +1,4 @@
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_AUTH = 'http://localhost:8000/api/auth/token_auth/';
 export const ENDPOINT_LISTING = 'http://localhost:8000/api/listing/';
+export const ENDPOINT_FAVORITE = 'http://localhost:8000/api/listing/favorite/';
