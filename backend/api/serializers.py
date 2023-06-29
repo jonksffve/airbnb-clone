71c7eff7f6d52287569e16c90692095a966f5ef5
Jonathan Ramirez <jonksff@gmail.com>
2023-06-29 14:50:19 -0400

    ADD: favorite serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Listing, Category
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import Listing, Category, FavoriteListing
 from accounts.serializers import UserSerializer
 
 
@@ -12,8 +12,22 @@ class CategorySerializer(ModelSerializer):
 class ListingSerializer(ModelSerializer):
     category = CategorySerializer(read_only=True)
     creator = UserSerializer(read_only=True)
+    is_liked = SerializerMethodField(read_only=True)
 
     class Meta:
         model = Listing
         fields = ['id', 'title', 'description', 'image', 'price', 'guestCount',
-                  'roomCount', 'bathroomCount', 'category', 'location', 'creator']
+                  'roomCount', 'bathroomCount', 'category', 'location', 'creator', 'is_liked']
+
+    def get_is_liked(self, obj):
+        user = self.context['request'].user
+        qs = obj.favorites.filter(user=user)
+        if qs.exists():
+            return True
+        return False
+
+
+class FavoriteSerializer(ModelSerializer):
+    class Meta:
+        model = FavoriteListing
+        fields = ['id', 'listing', 'user']
