fa2295f1e23e3f90b6833a8e91557bc2f9f3666f
Jonathan Ramirez <jonksff@gmail.com>
2023-07-01 15:17:03 -0400

    ADD: reservation listing to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Listing, FavoriteListing
+from .models import Category, Listing, FavoriteListing, ReservationListing
 
 
 admin.site.register(Category)
 admin.site.register(Listing)
 admin.site.register(FavoriteListing)
+admin.site.register(ReservationListing)
