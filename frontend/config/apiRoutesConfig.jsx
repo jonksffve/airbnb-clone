14e4be7641cbf2e0a82f456510ec5047bebe296c
Jonathan Ramirez <jonksff@gmail.com>
2023-07-01 22:40:40 -0400

    ADD: reservation endpoint

--- frontend/config/apiRoutesConfig.jsx
+++ frontend/config/apiRoutesConfig.jsx
@@ -1,4 +1,5 @@
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_AUTH = 'http://localhost:8000/api/auth/token_auth/';
 export const ENDPOINT_LISTING = 'http://localhost:8000/api/listing/';
+export const ENDPOINT_RESERVATION = 'http://localhost:8000/api/reservation/';
 export const ENDPOINT_FAVORITE = 'http://localhost:8000/api/listing/favorite/';
