fbe9347116acb5a9265c9b269fbee53bf8363229
Jonathan Ramirez <jonksff@gmail.com>
2023-07-02 20:58:38 -0400

    EDIT: updated to display reservation info

--- frontend/components/Listings/ListingCard.jsx
+++ frontend/components/Listings/ListingCard.jsx
@@ -1,16 +1,32 @@
 import HeartButton from '../UIhelpers/HeartButton';
 import { getByValue } from '../../hooks/WorldCountries';
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
+import { format } from 'date-fns';
+import Button from '../UIhelpers/Button';
 
-const ListingCard = ({ data, token }) => {
-	const reservationDate = undefined;
+const ListingCard = ({
+	data,
+	token,
+	isReservation = false,
+	onCancel,
+	actionID,
+}) => {
 	const fullLocation = getByValue(data.location);
+	const reservationDate = useMemo(() => {
+		if (!isReservation) return undefined;
+
+		const startDate = new Date(data.start_date);
+		const endDate = new Date(data.end_date);
+
+		return `${format(startDate, 'PP')} - ${format(endDate, 'PP')}`;
+	}, [isReservation, data.end_date, data.start_date]);
 
 	return (
 		<div className='group col-span-1 cursor-pointer'>
 			<div className='flex w-full flex-col gap-2'>
 				<div className='relative aspect-square w-full overflow-hidden rounded-xl'>
-					<Link to={`${data.id}`}>
+					<Link to={`/${data.id}`}>
 						<img
 							className='h-full w-full object-cover transition group-hover:scale-110'
 							src={data.image}
@@ -29,12 +45,21 @@ const ListingCard = ({ data, token }) => {
 					{fullLocation?.label}, {fullLocation?.region}
 				</div>
 				<div className='text-sm font-light text-neutral-500'>
-					{reservationDate || data.category.name}
+					{reservationDate || data.category?.name}
 				</div>
 				<div className='flex flex-row items-center gap-1'>
 					<div className='font-semibold'>$ {data.price}</div>
 					{!reservationDate && <div className='font-light'>/ night</div>}
 				</div>
+				{isReservation && new Date(data.end_date) > new Date() && (
+					<Button
+						small
+						label='Cancel'
+						onClick={() => {
+							onCancel(actionID);
+						}}
+					/>
+				)}
 			</div>
 		</div>
 	);
