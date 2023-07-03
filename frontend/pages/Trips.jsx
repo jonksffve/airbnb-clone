e1a6363d8fb3b4b21d01d9ee95ed37ab67256f20
Jonathan Ramirez <jonksff@gmail.com>
2023-07-03 01:00:53 -0400

    EDIT: spinner for loading state
    EDIT: useMemo for fetching
    
    EDIT: useCallback

--- frontend/pages/Trips.jsx
+++ frontend/pages/Trips.jsx
@@ -1,38 +1,48 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useCallback, useMemo, useState } from 'react';
 import Heading from '../components/Modals/Heading';
 import Container from '../components/UIhelpers/Container';
 import { useSelector } from 'react-redux';
-import { getListingReservationsAPI } from '../api/AuthAPI';
+import {
+	deleteReservationAPI,
+	getListingReservationsAPI,
+} from '../api/AuthAPI';
 import ListingCard from '../components/Listings/ListingCard';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '../components/UIhelpers/Spinner';
 
 useSelector;
 
 const Trips = () => {
 	const [tripsData, setTripsData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(undefined);
 	const user = useSelector((state) => state.user);
+	const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchTrips = async () => {
-			if (!user.token) return;
-			await getListingReservationsAPI(user.token, setTripsData, setIsLoading);
-		};
-
-		fetchTrips();
+	useMemo(async () => {
+		if (!user.token) return;
+		await getListingReservationsAPI(user.token, setTripsData, setIsLoading);
 	}, [user.token]);
 
-	const handleCancel = (reservationID) => {
-		console.log('clicked cancel', reservationID);
-	};
+	const handleCancel = useCallback(
+		async (reservationID) => {
+			await deleteReservationAPI(reservationID, user.token).then(() => {
+				navigate(0);
+			});
+		},
+		[user.token, navigate]
+	);
 
 	return (
 		<Container>
-			<Heading
-				title='All your trips'
-				subtitle='Where you have been (and where you will be...)'
-			/>
-			<div
-				className='mt-10
+			{isLoading && <Spinner />}
+			{!isLoading && (
+				<Fragment>
+					<Heading
+						title='All your trips'
+						subtitle='Where you have been (and where you will be...)'
+					/>
+					<div
+						className='mt-10
                         grid 
                         grid-cols-1 
                         gap-8 
@@ -42,22 +52,25 @@ const Trips = () => {
                         xl:grid-cols-5
                         2xl:grid-cols-6
                         '
-			>
-				{tripsData.map((trip) => (
-					<ListingCard
-						key={trip.id}
-						data={{
-							start_date: trip.start_date,
-							end_date: trip.end_date,
-							...trip.listing,
-						}}
-						token={user.token}
-						isReservation
-						onCancel={handleCancel}
-						actionID={trip.id}
-					/>
-				))}
-			</div>
+					>
+						{tripsData.map((trip) => (
+							<ListingCard
+								key={trip.id}
+								data={{
+									start_date: trip.start_date,
+									end_date: trip.end_date,
+									...trip.listing,
+								}}
+								token={user.token}
+								isReservation
+								onCancel={handleCancel}
+								actionID={trip.id}
+								isLoading={isLoading}
+							/>
+						))}
+					</div>
+				</Fragment>
+			)}
 		</Container>
 	);
 };
