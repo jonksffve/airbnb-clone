f69de916431dc47ccdd0d0355e195f7672c8d284
Jonathan Ramirez <jonksff@gmail.com>
2023-07-02 20:56:02 -0400

    ADD: final paths

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
 import Index from '../../pages/Index';
 import ListingDetail from '../../pages/ListingDetail';
+import Trips from '../../pages/Trips';
 
 export const router = createBrowserRouter([
 	{
@@ -15,6 +16,22 @@ export const router = createBrowserRouter([
 			{
 				path: ':listingID',
 				element: <ListingDetail />,
+			},
+			{
+				path: 'trips/',
+				element: <Trips />,
+			},
+			{
+				path: 'favorites/',
+				element: <h2>My favorites</h2>,
+			},
+			{
+				path: 'reservations/',
+				element: <h2>My reservations</h2>,
+			},
+			{
+				path: 'properties/',
+				element: <h2>My properties</h2>,
 			},
 		],
 	},
