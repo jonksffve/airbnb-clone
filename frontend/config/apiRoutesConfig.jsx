dc99d5226caa0d68379e5695927ac2872ddd31ec
Jonathan Ramirez <jonksff@gmail.com>
2023-07-02 21:00:00 -0400

    ADD: final url endpoints

--- frontend/config/apiRoutesConfig.jsx
+++ frontend/config/apiRoutesConfig.jsx
@@ -1,5 +1,12 @@
+//API URLS ENDPOINTS
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_AUTH = 'http://localhost:8000/api/auth/token_auth/';
 export const ENDPOINT_LISTING = 'http://localhost:8000/api/listing/';
 export const ENDPOINT_RESERVATION = 'http://localhost:8000/api/reservation/';
 export const ENDPOINT_FAVORITE = 'http://localhost:8000/api/listing/favorite/';
+//APPLICATION URLS ROUTES
+export const ROUTE_HOME = '/';
+export const ROUTE_TRIPS = 'trips/';
+export const ROUTE_FAVORITES = 'favorites/';
+export const ROUTE_RESERVATIONS = 'reservations/';
+export const ROUTE_PROPERTIES = 'properties/';
