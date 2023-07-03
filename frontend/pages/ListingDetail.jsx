0d41647f60cf0d4de4416920d2193b780c4427a2
Jonathan Ramirez <jonksff@gmail.com>
2023-07-03 00:58:46 -0400

    EDIT: useMemo to fetch data
    ADD: spinner for loading logic

--- frontend/pages/ListingDetail.jsx
+++ frontend/pages/ListingDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import ListingHeader from '../components/Listings/ListingHeader';
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
 	getListingInformationAPI,
@@ -9,73 +9,73 @@ import {
 import Container from '../components/UIhelpers/Container';
 import ListingInfo from '../components/Listings/ListingInfo';
 import ListingReservation from '../components/Listings/ListingReservations';
+import Spinner from '../components/UIhelpers/Spinner';
 
 const ListingDetail = () => {
 	const { listingID } = useParams();
 	const [listing, setListing] = useState({});
 	const [reservations, setReservations] = useState([]);
 	const user = useSelector((state) => state.user);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(undefined);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			if (!user.token) return;
-			await getListingInformationAPI(listingID, user.token, setListing);
-			await getListingReservationsAPI(
-				user.token,
-				setReservations,
-				setIsLoading,
-				listingID
-			);
-		};
-
-		fetchData();
+	useMemo(async () => {
+		if (!user.token) return;
+		await getListingInformationAPI(listingID, user.token, setListing);
+		await getListingReservationsAPI(
+			user.token,
+			setReservations,
+			setIsLoading,
+			listingID
+		);
 	}, [listingID, user.token]);
 
 	return (
 		<Container>
-			<div className='mx-auto max-w-screen-lg'>
-				<div className='flex flex-col gap-6'>
-					<ListingHeader
-						token={user.token}
-						title={listing.title}
-						image={listing.image}
-						locationValue={listing.location}
-						id={listing.id}
-						liked={listing.is_liked}
-					/>
-					<div
-						className='mt-6
-					grid
-					grid-cols-1
-					md:grid-cols-7
-					md:gap-10'
-					>
-						<ListingInfo
-							creator={listing.creator}
-							category={listing.category}
-							description={listing.description}
-							roomCount={listing.roomCount}
-							guestCount={listing.guestCount}
-							bathroomCount={listing.bathroomCount}
+			{isLoading && <Spinner />}
+			{!isLoading && (
+				<div className='mx-auto max-w-screen-lg'>
+					<div className='flex flex-col gap-6'>
+						<ListingHeader
+							token={user.token}
+							title={listing.title}
+							image={listing.image}
 							locationValue={listing.location}
+							id={listing.id}
+							liked={listing.is_liked}
 						/>
 						<div
-							className='order-first
-						mb-10
-						md:order-last
-						md:col-span-3'
+							className='mt-6
+				grid
+				grid-cols-1
+				md:grid-cols-7
+				md:gap-10'
 						>
-							<ListingReservation
-								token={user.token}
-								listingID={listing.id}
-								price={listing.price}
-								reservations={reservations}
+							<ListingInfo
+								creator={listing.creator}
+								category={listing.category}
+								description={listing.description}
+								roomCount={listing.roomCount}
+								guestCount={listing.guestCount}
+								bathroomCount={listing.bathroomCount}
+								locationValue={listing.location}
 							/>
+							<div
+								className='order-first
+					mb-10
+					md:order-last
+					md:col-span-3'
+							>
+								<ListingReservation
+									token={user.token}
+									listingID={listing.id}
+									price={listing.price}
+									reservations={reservations}
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</Container>
 	);
 };
