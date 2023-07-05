2f18bdce9842afed38a04705651d06afd839d5dc
Jonathan Ramirez <jonksff@gmail.com>
2023-07-05 11:29:33 -0400

    EDIT: ordering by start_date

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,8 +4,9 @@ from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
-    name = models.CharField(_("Category"), max_length=50,
-                            blank=False, null=False, unique=True)
+    name = models.CharField(
+        _("Category"), max_length=50, blank=False, null=False, unique=True
+    )
 
     class Meta:
         verbose_name = _("Category")
@@ -16,26 +17,47 @@ class Category(models.Model):
 
 
 class Listing(models.Model):
-    title = models.CharField(_("Title"), max_length=50,
-                             blank=False, null=False)
+    title = models.CharField(_("Title"), max_length=50, blank=False, null=False)
     description = models.TextField(_("Description"), blank=False, null=False)
     image = models.ImageField(
-        _("Cover image"), upload_to='listings/', default='placeholder.jpeg', blank=False, null=False)
+        _("Cover image"),
+        upload_to="listings/",
+        default="placeholder.jpeg",
+        blank=False,
+        null=False,
+    )
     price = models.DecimalField(
-        max_digits=6, decimal_places=2, blank=False, null=False, validators=[MinValueValidator(1)])
+        max_digits=6,
+        decimal_places=2,
+        blank=False,
+        null=False,
+        validators=[MinValueValidator(1)],
+    )
     guestCount = models.SmallIntegerField(
-        _("Guests count"), blank=False, null=False, validators=[MinValueValidator(1)])
+        _("Guests count"), blank=False, null=False, validators=[MinValueValidator(1)]
+    )
     roomCount = models.SmallIntegerField(
-        _("Room count"), blank=False, null=False, validators=[MinValueValidator(1)])
+        _("Room count"), blank=False, null=False, validators=[MinValueValidator(1)]
+    )
     bathroomCount = models.SmallIntegerField(
-        _("Bathroom count"), blank=False, null=False, validators=[MinValueValidator(1)])
-    location = models.CharField(
-        _("Location"), max_length=50, blank=False, null=False)
+        _("Bathroom count"), blank=False, null=False, validators=[MinValueValidator(1)]
+    )
+    location = models.CharField(_("Location"), max_length=50, blank=False, null=False)
     created_at = models.DateTimeField(_("Date"), auto_now_add=True, blank=True)
     creator = models.ForeignKey(
-        "accounts.User", verbose_name=_("Creator"), on_delete=models.CASCADE, blank=False, null=False)
-    category = models.ForeignKey("Category", verbose_name=_(
-        "Category"), on_delete=models.CASCADE, blank=False, null=False)
+        "accounts.User",
+        verbose_name=_("Creator"),
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    category = models.ForeignKey(
+        "Category",
+        verbose_name=_("Category"),
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
 
     class Meta:
         verbose_name = "Listing"
@@ -46,30 +68,43 @@ class Listing(models.Model):
 
 
 class FavoriteListing(models.Model):
-    listing = models.ForeignKey(Listing, verbose_name=_(
-        ""), related_name='favorites', on_delete=models.CASCADE)
+    listing = models.ForeignKey(
+        Listing, verbose_name=_(""), related_name="favorites", on_delete=models.CASCADE
+    )
     user = models.ForeignKey(
-        "accounts.User", verbose_name=_(""), on_delete=models.CASCADE)
+        "accounts.User", verbose_name=_(""), on_delete=models.CASCADE
+    )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['listing', 'user'], name='unique_listing_user')
+                fields=["listing", "user"], name="unique_listing_user"
+            )
         ]
 
     def __str__(self):
-        return f'{self.user.get_full_name()} favorites {self.listing.title}'
+        return f"{self.user.get_full_name()} favorites {self.listing.title}"
 
 
 class ReservationListing(models.Model):
-    listing = models.ForeignKey(Listing, verbose_name=_(
-        ""), related_name="revervations", on_delete=models.CASCADE)
+    listing = models.ForeignKey(
+        Listing,
+        verbose_name=_(""),
+        related_name="revervations",
+        on_delete=models.CASCADE,
+    )
     user = models.ForeignKey(
-        "accounts.User", verbose_name=_(""), on_delete=models.CASCADE)
+        "accounts.User", verbose_name=_(""), on_delete=models.CASCADE
+    )
     start_date = models.DateTimeField(
-        _(""), blank=False, null=False, auto_now=False, auto_now_add=False)
+        _(""), blank=False, null=False, auto_now=False, auto_now_add=False
+    )
     end_date = models.DateTimeField(
-        _(""), blank=False, null=False, auto_now=False, auto_now_add=False)
+        _(""), blank=False, null=False, auto_now=False, auto_now_add=False
+    )
 
     def __str__(self):
         return f"{self.user.get_full_name()} reservated: {self.listing.title}"
+
+    class Meta:
+        ordering = ["start_date"]
