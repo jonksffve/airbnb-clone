5368fb5b167288a50c3b477689f11083148e7c1b
Jonathan Ramirez <jonksff@gmail.com>
2023-07-04 20:02:22 -0400

    EDIT: dynamically setting values based on search

--- frontend/components/Menu/Search.jsx
+++ frontend/components/Menu/Search.jsx
@@ -1,15 +1,69 @@
+import { useCallback, useMemo, useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
+import { useSearchParams } from 'react-router-dom';
+import { getByValue } from '../../hooks/WorldCountries';
+import { differenceInDays } from 'date-fns';
 
 const Search = () => {
+	const dispatch = useDispatch();
+	const handleSearch = useCallback(() => {
+		dispatch(uiActions.showSearchModal());
+	}, [dispatch]);
+
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [where, setWhere] = useState('Anywhere');
+	const [guestCount, setGuestCount] = useState('Add guests');
+	const [dayCount, setDayCount] = useState('Any time');
+
+	useMemo(() => {
+		if (searchParams.get('location')) {
+			const location = getByValue(searchParams.get('location'));
+			setWhere(location.label);
+		} else {
+			setWhere('Anywhere');
+		}
+
+		if (searchParams.get('guestCount')) {
+			let value = '';
+			value =
+				searchParams.get('guestCount') === '1'
+					? '1 guest'
+					: `${searchParams.get('guestCount')} guests`;
+			setGuestCount(value);
+		} else {
+			setGuestCount('Add guests');
+		}
+
+		if (searchParams.get('startDate') && searchParams.get('endDate')) {
+			const days = differenceInDays(
+				new Date(searchParams.get('endDate')),
+				new Date(searchParams.get('startDate'))
+			);
+
+			let value = '';
+
+			value = days === 1 ? '1 day' : `${days} days`;
+
+			setDayCount(value);
+		} else {
+			setDayCount('Any time');
+		}
+	}, [searchParams]);
+
 	return (
-		<div className='w-full cursor-pointer rounded-full border-[1px] py-2 shadow-sm transition hover:shadow-md md:w-auto'>
+		<div
+			className='w-full cursor-pointer rounded-full border-[1px] py-2 shadow-sm transition hover:shadow-md md:w-auto'
+			onClick={handleSearch}
+		>
 			<div className='flex flex-row items-center justify-between'>
-				<div className='px-6 text-sm font-semibold'>Anywhere</div>
+				<div className='px-6 text-sm font-semibold'>{where}</div>
 				<div className='hidden flex-1 border-x-[1px] px-6 text-center text-sm font-semibold sm:block'>
-					Any week
+					{dayCount}
 				</div>
 				<div className='flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600'>
-					<div className='hidden sm:block'>Add Guests</div>
+					<div className='hidden sm:block'>{guestCount}</div>
 					<div className='rounded-full bg-rose-500 p-2 text-white'>
 						<BiSearch size={18} />
 					</div>
